474644b051d13e0d8c35ae6d2f73e29b852f7dfa
Daniel Ivanov <mr.dan6886@ya.ru>
2022-09-17 16:43:12 +0300

    adding express

--- request_to_api.js
+++ request_to_api.js
@@ -1,1 +1,11 @@
-//this is JS BITCH
+//this is JS BITCHadd
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
