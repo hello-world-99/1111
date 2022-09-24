a7afc21e3afec65bd8d57bc0e18fbf63e6ec8595
hello-world-99 <theraincmpunk@gmail.com>
2022-09-24 13:23:50 +0300

    button3

--- request_to_api.js
+++ request_to_api.js
@@ -71,5 +71,5 @@ app.post('/lev_button', function(req, res) {
         }
     )
 
-    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ/');
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ/","_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
 });
